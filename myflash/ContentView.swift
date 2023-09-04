95603a74a13f5bd01365c76ef8f172accdf2f675
Zahirudeen Premji <headydiscy@gmail.com>
2023-09-04 08:31:54 -0600

    Initial Commit

---
+++ myflash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myflash
+//
+//  Created by Zahirudeen Premji on 9/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myflash/myflashApp.swift
@@ -0,0 +1,17 @@
+//
+//  myflashApp.swift
+//  myflash
+//
+//  Created by Zahirudeen Premji on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct myflashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
